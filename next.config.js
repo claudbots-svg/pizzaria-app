4457ce3ba6010f41df861802a2f04f2e86bc182a
claudbots-svg <claudbots@gmail.com>
2025-10-01 11:24:39 -0300

    adicionando supabaseClient e globals.css

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["ufowwpcsuwciktyffflj.supabase.co"], // supabase imagens
+  },
+};
+
+module.exports = nextConfig;
